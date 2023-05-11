cfcf4748aa9cf87d287e20992ffcd44b9fe3ed97
Brandon Taylor <bmt1994.tlh@gmail.com>
2023-05-11 11:53:47 -0400

    more

--- index.js
+++
@@ -1,8 +0,0 @@
-const { Client } = require("pg"); // imports the pg module
-
-// supply the db name and location of the database
-const client = new Client("postgres://localhost:5432/juicebox-dev");
-
-module.exports = {
-	client,
-};
